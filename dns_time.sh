15a8ff9dc11110b2c1ac8e167d5373e1bda6b981
rodrigo-kondo <rodi.kondo@gmail.com>
2018-04-07 10:17:06 -0300

    dns_time.sh improved

--- dns_time.sh
+++ dns_time.sh
@@ -1,14 +1,35 @@
-#!/bin/sh
-
-alias dig='/usr/bin/dig'
-
-
-for domain in $(cat domains.txt)
+#!/bin/bash
+if [ -f .tmp ]; then 
+	read -r -p "this script will erase \".tmp\"file Are you sure? [y/N] " response
+	case "$response" in
+    	[yY][eE][sS]|[yY]) 
+         	: > .tmp
+	;;
+    	*)
+		exit
+	;;
+	esac
+fi
+echo 
+ while read -r domain
 do
-    for server in $(cat servers.txt)
+    while read -r server
     do
-        response_time=$(dig @$server $domain | grep -o "Query time: [0-9]*" | grep -o "[0-9]*")
-        echo $server,$domain,$response_time
-    done
-done
+        response_time="$(drill @$server $domain | grep -o "Query time: [0-9]*" | grep -o "[0-9]*")"
+        echo " $server-> $domain::$response_time "
+        echo "$server,$domain,$response_time " >> .tmp
+    done < servers.txt
+    echo
+done < domains.txt
+	echo
+	while read -r server
+	do
+		echo "$server ->>>>"
+		echo -n "avg:"
+		echo "$(< .tmp grep "$server" |cut -d ',' -f 3|awk 'NF{sum+=$1} END {print sum}')"/"$(wc -l domains.txt|grep -o "[0-9]*")"| bc 
+		< .tmp grep "$server" |cut -d ',' -f 3|sort -n |echo min:"$(head -1)" 
+		< .tmp grep "$server" |cut -d ',' -f 3|sort -n |echo max:"$(tail -1)"
+		echo 
+	done <servers.txt
+rm .tmp 
 exit
