56189db090766a8e245afd629492a253a8c5ee13
rodrigo-kondo <rodi.kondo@gmail.com>
2018-04-26 18:50:20 -0300

    using variables instead of wrinting on disk

--- dns_time.sh
+++ dns_time.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
-if [ -f .tmp ]; then 
-	read -r -p "this script will erase \".tmp\"file Are you sure? [y/N] " response
-	case "$response" in
-    	[yY][eE][sS]|[yY]) 
-         	: > .tmp
-	;;
-    	*)
-		exit
-	;;
-	esac
-fi
-echo 
- while read -r domain
+TMP= :
+while read -r domain
 do
     while read -r server
     do
         response_time="$(drill @$server $domain | grep -o "Query time: [0-9]*" | grep -o "[0-9]*")"
-        echo " $server-> $domain::$response_time "
-        echo "$server,$domain,$response_time " >> .tmp
+        echo "$server-> $domain::$response_time"
+        TMP+="$(echo "$server,$domain,$response_time\n")"
     done < servers.txt
     echo
 done < domains.txt
 	echo
+	echo -e $TMP
 	while read -r server
 	do
 		echo "$server ->>>>"
 		echo -n "avg:"
-		echo "$(< .tmp grep "$server" |cut -d ',' -f 3|awk 'NF{sum+=$1} END {print sum}')"/"$(wc -l domains.txt|grep -o "[0-9]*")"| bc 
-		< .tmp grep "$server" |cut -d ',' -f 3|sort -n |echo min:"$(head -1)" 
-		< .tmp grep "$server" |cut -d ',' -f 3|sort -n |echo max:"$(tail -1)"
+		echo "$(echo -e $TMP|grep $server |cut -d ',' -f 3|awk 'NF{sum+=$1} END {print sum}')"/"$(wc -l domains.txt|grep -o "[0-9]*")"|bc 
+		echo -e $TMP |grep "$server" |cut -d ',' -f 3|sort -n |echo min:"$(head -1)" 
+		echo -e $TMP|grep "$server" |cut -d ',' -f 3|sort -n |echo max:"$(tail -1)"
+		echo -e $TMP|grep "$server" |cut -d ',' -f 3|sort -n |uniq -c |awk '{$2=$2"->"$1;$1="";print} '
 		echo 
 	done <servers.txt
-rm .tmp 
 exit
